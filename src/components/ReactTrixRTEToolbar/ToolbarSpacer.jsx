f1c2ccba9490c32f61e81e9db3b5aa58f633e031
Abhay Nikam <nikam.abhay1@gmail.com>
2020-03-29 18:30:11 +0530

    Fixed the classname react warning

--- src/components/ReactTrixRTEToolbar/ToolbarSpacer.jsx
+++ src/components/ReactTrixRTEToolbar/ToolbarSpacer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ToolbarSpacer() {
-  return(<span class="trix-button-group-spacer"></span>);
+  return(<span className="trix-button-group-spacer"></span>);
 }
