89fbd9eb73e03ead3034d696dfb7fd11bfcad27a
Abhay Nikam <nikam.abhay1@gmail.com>
2020-04-05 23:32:24 +0530

    Added storybook for ReactTrixRTE live demo

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          {
+            test: /\.(scss)$/,
+            loader: 'style-loader!css-loader!sass-loader'
+          },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            include: path.join(__dirname, '../stories'),
+            use: [
+              {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+              }
+            ]
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+      },
+    }
+  }
+};
---
+++ stories/0-ReactTrixInputRTE.stories.js
@@ -0,0 +1,72 @@
+import React, { useRef } from 'react';
+import { action } from '@storybook/addon-actions';
+import { ReactTrixRTEInput } from '../index';
+
+export default {
+  title: 'React Trix Editor',
+  component: ReactTrixRTEInput,
+};
+
+function ReactTrixInputRefComponent() {
+  const reactTrixInputRef = useRef()
+  const handleInitialize = (event) => {
+    reactTrixInputRef.current.editor.insertHTML("<strong>Added using trixInputRef programmatically!</strong>");
+  }
+
+  return (
+    <ReactTrixRTEInput
+      trixInputRef={reactTrixInputRef}
+      onInitialize={handleInitialize}
+    />
+  )
+}
+
+export const DefaultReactTrixEditor = () => (
+  <ReactTrixRTEInput />
+);
+
+export const WithDefaultValue = () => (
+  <ReactTrixRTEInput
+    defaultValue='<h1>React Trix Rich Text Editor</h1><div>Trix is an open-source project from Basecamp, the creators of Ruby on Rails. Millions of people trust their text to Basecamp, and we built Trix to give them the best possible editing experience.</div><br /><div><strong>React Trix RTE wrapper to Trix Editor was created by: </strong><a href="https://www.abhaynikam.me/">Abhay Nikam</a></div>'
+  />
+);
+
+export const WithTrixInputRef = () => (
+  <ReactTrixInputRefComponent />
+)
+
+export const WithOnChangeHandler = () => (
+  <ReactTrixRTEInput onChange={action('onChange')} />
+);
+
+export const WithOnBlur = () => (
+  <ReactTrixRTEInput onBlur={action('onBlur')} />
+);
+
+export const WithOnFocus = () => (
+  <ReactTrixRTEInput onFocus={action('onFocus')} />
+);
+
+export const WithBeforeInitialize = () => (
+  <ReactTrixRTEInput onBeforeInitialize={action('onBeforeInitialize')} />
+);
+
+export const WithInitialize = () => (
+  <ReactTrixRTEInput onInitialize={action('onInitialize')} />
+);
+
+export const WithSelectionChange = () => (
+  <ReactTrixRTEInput onSelectionChange={action('onSelectionChange')} />
+);
+
+export const WithFileAccepted = () => (
+  <ReactTrixRTEInput onFileAccepted={action('onFileAccepted')} />
+);
+
+export const WithAttachmentAdd = () => (
+  <ReactTrixRTEInput onAttachmentAdd={action('onAttachmentAdd')} />
+);
+
+export const WithAttachmentRemove = () => (
+  <ReactTrixRTEInput onAttachmentRemove={action('onAttachmentRemove')} />
+);
