fab45c99579c4e0d51f8baabf1fe45e3918b4115
alejo4373 <alejandrofranco@pursuit.org>
2021-05-12 00:04:26 -0400

    Add story with ReactTrixInputRTE using a className prop

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-cssresources'],
   webpackFinal: (config) => {
     return {
       ...config,
